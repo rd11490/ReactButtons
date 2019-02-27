51c8a50e839b943bd84da9bcf0a09f5162c115b1
Ryan Davis <rd11490@gmail.com>
2019-02-26 23:46:49 -0500

    Some organization

---
+++ src/RadioGroup.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+class RadioButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="radio"
+                       value={this.props.option.value}
+                       name={this.props.option.label}
+                       checked={this.props.isChecked}
+                       onClick={this.props.onClick}>
+                </input>
+                <label>
+                    {this.props.option.label}
+
+                </label>
+            </div>
+        );
+    }
+}
+
+export class RadioGroup extends React.Component {
+    constructor(props) {
+        const radioOptions = [
+            {label: 'Label 1', value: 1},
+            {label: 'Label 2', value: 2},
+            {label: 'Label 3', value: 3},
+            {label: 'Label 4', value: 4}
+        ];
+
+        super(props);
+        this.state = {
+            options: radioOptions,
+            selected: radioOptions[0]
+        }
+    }
+
+    setSelected(option) {
+        this.setState({
+            options: this.state.options,
+            selected: option
+        })
+    }
+
+    renderButtonn(option) {
+        return (
+            <RadioButton
+                option={option}
+                onClick={() => this.setSelected(option)}
+                isChecked={this.state.selected === option}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                The Current selection is: {this.state.selected.label}
+                <div>
+                    {this.state.options.slice().map(option => this.renderButtonn(option))}
+                </div>
+            </div>
+        );
+    }
+}

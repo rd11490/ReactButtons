04a6ec9715569c4031937ec14b541aef2fb54766
Ryan Davis <rd11490@gmail.com>
2019-02-26 23:41:01 -0500

    Creating RadioGroup code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+class RadioButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="radio"
+                       value={this.props.option.value}
+                       name={this.props.option.label}
+                       checked={this.props.isChecked}
+                       onClick={this.props.onClick}>
+                </input>
+                <label>
+                    {this.props.option.label}
+
+                </label>
+            </div>
+        );
+    }
+}
+
+class RadioGroup extends React.Component {
+    constructor(props) {
+        const radioOptions = [
+            {label: 'Label 1', value: 1},
+            {label: 'Label 2', value: 2},
+            {label: 'Label 3', value: 3},
+            {label: 'Label 4', value: 4}
+        ];
+
+        super(props);
+        this.state = {
+            options: radioOptions,
+            selected: radioOptions[0]
+        }
+    }
+
+    setSelected(option) {
+        this.setState({
+            options: this.state.options,
+            selected: option
+        })
+    }
+
+    renderButtonn(option) {
+        return (
+            <RadioButton
+                option={option}
+                onClick={() => this.setSelected(option)}
+                isChecked={this.state.selected === option}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                The Current selection is: {this.state.selected.label}
+                <div>
+                    {this.state.options.slice().map(option => this.renderButtonn(option))}
+                </div>
+            </div>
+        );
+    }
+}
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="RadioGroup">
+                <RadioGroup/>
+            </div>
+        );
+    }
 }
 
 export default App;
